92b68f476205cc86f0e10da6e87ad73230e67ca5
nanutra <nicolas.mendiboure@gmail.com>
2022-04-12 11:26:46 +0200

    car prefab

---
+++ Assets/Scripts/NetworkManagerCar.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+namespace Mirror.Examples.Pong
+{
+
+        [AddComponentMenu("")]
+    public class NetworkManagerCar : NetworkManager
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+
+}
